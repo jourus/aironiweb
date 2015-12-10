7cf86fd8d922bcee01dd6f18447186345d14f9d5
jourus <gianfranco.ferracci@gmail.com>
2015-12-10 01:07:33 +0100

    0.1.6
    Il test dell’autofoto ora funziona alla grande, compresa la
    temporizzazione.
    Va portato nel file definitivo con le impostazioni corrette per i
    percorsi.

--- site/api/test.php
+++ site/api/test.php
@@ -48,7 +48,7 @@ function getElencoFoto() {
 	
 }
 
-
+function refreshFoto() {
 	// http://localhost/airo/api/?method=elencofoto&format=json
 	// http://localhost/airo/api/?method=layoutfoto&format=json
 	var urlOpzioni=  '../api/?method=layoutfoto&format=json';
@@ -61,34 +61,63 @@ function getElencoFoto() {
 
 		var Posizioni = data.data.layout[scelto].foto;
 
-		var ElencoFoto = getElencoFoto();
-		if (ElencoFoto.length == 0) {
-			return ;
-			}
+		var ElencoFoto= [];
 		
-		while (ElencoFoto.length < Posizioni.length){
+		if(sessionStorage.ElencoFoto)
+		{	
+			ElencoFoto = JSON.parse(sessionStorage.ElencoFoto);
+		}
+		//var ElencoFoto = getElencoFoto();
+		//if (ElencoFoto.length == 0) {
+		//	return ;
+		//	}
+		while (ElencoFoto.length < Posizioni.length) {
 
-				ElencoFoto.concat(getElencoFoto());
+				var bufferFoto = getElencoFoto();
+
+				if (bufferFoto.length == 0)
+					return;
+				
+				ElencoFoto = ElencoFoto.concat(bufferFoto);
+				
+				console.log(ElencoFoto.length);
 			}
-		
-		
+		var FotoPronte = [];
+
+		for (x=0; x < Posizioni.length; x++) {
+			FotoPronte.push(ElencoFoto.shift());	
+		}
+		$(".myItem").remove();
+		sessionStorage.ElencoFoto = JSON.stringify(ElencoFoto);
+		var y= 0;
 		$.each(Posizioni, function(key, layout) {
 
 			// Qui bisogna avere le immagini
 //			console.log(layout.top);
 
-			$("#theDiv").append("<div class='myItem' style='top: " + layout.top +"; left: " + layout.left + "; width: " + layout.width + "; height: " + layout.height + "; visibility: hidden;'></div>");
-;
+			$("#theDiv").append("<img class='myItem'  src='../immagini/fotolibere/" + FotoPronte[y] + "' style='top: " + layout.top +"; left: " + layout.left + "; width: " + layout.width + "; height: " + layout.height + "; display: none;'/>");
+			y++;		
 		});
-		$(".myItem").hide();
-		$(".myItem").css('visibility', 'visible');
-		
-		$(".myItem").fadeIn(3000)		
+
+
+
+
+		$(".myItem").fadeIn(2000)		
 	});
 
 
+}
+refreshFoto();	
+
+setInterval(function(){ 
+	$(".myItem").fadeOut(1500, "swing", function(){
+		
+		refreshFoto();	
+		});
 
 	
+	
+ }, 6000);
 
 
 
